62c714dd0c1d92257f7645679fd005a7270c5dec
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-03-29 13:17:08 -0700

    added context data to admin_settings view

--- building/views.py
+++ building/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
+from .models import *
 
 
 def admin_dashboard(request):
-    return render(request, 'building/dashboard.html')
+    building = Building.objects.first()
+    apartments = Apartment.objects.all().filter(building=building)
+    context = {
+        'building': building,
+        'occupied': len(apartments.filter(number_of_persons__gt=0)),
+        'apartments_all': apartments,
+    }
+
+    return render(request, 'building/dashboard.html', context)
 
 
 def admin_settings(request):
-    return render(request, 'building/settings.html')
+    user = User.objects.get(username='bogdan')
+    building = Building.objects.get(admin=user)
+
+    # Queryset for mutual utilities
+    mutual_utils = MutualUtil.objects.all().\
+        filter(common_util__building=building)
+
+    supply_data = IndividualUtil.objects.all().\
+        filter(individual_util__building=building).\
+        order_by('individual_util__name')
+
+    # Queryset for displaying all apartments and their settings
+    apartments = Apartment.objects.all().\
+        filter(building=building)
+
+    context = {
+        'mutual_utils': mutual_utils,
+        'supply_data': supply_data.order_by('individual_util__name'),
+        'apartments': apartments
+    }
+
+    return render(request, 'building/settings.html', context)
 
 
 def admin_counters(request):
