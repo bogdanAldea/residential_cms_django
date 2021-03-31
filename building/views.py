795b94e559df70d058cf4d3d9db6411dae10a127
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-03-31 15:14:58 -0700

    added building admin login, logout & building creation view

--- building/views.py
+++ building/views.py
@@ -1,21 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from .models import *
+from .forms import *
 
 
+# View for building's admin dashboard
 def admin_dashboard(request):
-    building = Building.objects.first()
-    apartments = Apartment.objects.all().filter(building=building)
+    user = User.objects.get(username=request.user.username)
+    try:
+        building = Building.objects.get(admin=user)
+    except Building.DoesNotExist:
+        return redirect('building:create_residential')
+
+    apartments = Apartment.objects.filter(building=building)
+    occupied = apartments.filter(number_of_persons__gt=0)
+
     context = {
         'building': building,
-        'occupied': len(apartments.filter(number_of_persons__gt=0)),
-        'apartments_all': apartments,
+        'apartments': apartments,
+        'occupied': occupied,
     }
 
     return render(request, 'building/dashboard.html', context)
 
 
+# View for building's admin settings
 def admin_settings(request):
-    user = User.objects.get(username='bogdan')
+    user = User.objects.get(username=request.user.username)
     building = Building.objects.get(admin=user)
 
     # Queryset for mutual utilities
@@ -26,6 +37,9 @@ def admin_settings(request):
         filter(individual_util__building=building).\
         order_by('individual_util__name')
 
+    features_data = FeatureLinked.objects.all().\
+        filter(related_feature__building=building)
+
     # Queryset for displaying all apartments and their settings
     apartments = Apartment.objects.all().\
         filter(building=building)
@@ -33,16 +47,64 @@ def admin_settings(request):
     context = {
         'mutual_utils': mutual_utils,
         'supply_data': supply_data.order_by('individual_util__name'),
-        'apartments': apartments
+        'features_data': features_data,
+        'apartments': apartments,
     }
 
     return render(request, 'building/settings.html', context)
 
 
+# View for building's admin counters
 def admin_counters(request):
     return render(request, 'building/counters.html')
 
 
+# View for building's admin apartment payments
 def admin_payments(request):
     return render(request, 'building/payments.html')
 
+
+# View for building admin creation/registration
+def register_page(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('building:login')
+
+    context = {'form': form}
+    return render(request, 'building/register.html', context)
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('building:dashboard')
+
+    return render(request, 'building/login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('building:login')
+
+
+# View for building creation when building queryset is 0.
+# Redirected here form admin login is requested admin has no building under administration
+def add_residential(request):
+    form = CreateResidentialForm()
+
+    if request.method == 'POST':
+        form = CreateResidentialForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('building:dashboard')
+    context = {'form': form}
+    return render(request, 'building/create_residential.html', context)
