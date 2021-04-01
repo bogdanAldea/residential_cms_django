69f2fa944e9b97803bbbe0530dcb25dbff41e157
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-04-01 11:25:28 -0700

    added default choice to utility create bulk

--- building/signals.py
+++ building/signals.py
@@ -1,15 +1,30 @@
 from django.db.models.signals import post_save
-from .models import Building, Apartment, User
+from .models import Building, Apartment, Utility, IndividualUtil
+from .config import UtilType
 
 
 def generate_apartments(sender, instance, created, **kwargs):
     if created:
+        utils_to_create = [
+            Utility(name='Cold water', provider='City', building=instance, util_type=UtilType.individual),
+            Utility(name='Hot water', provider='City', building=instance, util_type=UtilType.individual),
+            Utility(name='Gas power', provider='City', building=instance, util_type=UtilType.individual),
+            Utility(name='Heating power', provider='City', building=instance, util_type=UtilType.individual)
+        ]
+        Utility.objects.bulk_create(utils_to_create)
+
         apartment_to_create = instance.number_of_apartments
+        utilities = Utility.objects.filter(building=instance)
         for number in range(1, apartment_to_create+1):
-            Apartment.objects.create(
+            apartment = Apartment.objects.create(
                 id=number,
                 building=instance
             )
+            for util in utilities:
+                IndividualUtil.objects.create(
+                    apartment=apartment,
+                    individual_util=util
+                )
 
 
 post_save.connect(generate_apartments, sender=Building)
