c242cc4ec92105bd94332dfb57981eba05ae3598
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-04-01 18:14:17 -0700

    added url pattern for utility creation form

--- building/urls.py
+++ building/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
 
-    path('create-residential/', views.add_residential, name='create_residential')
+    path('create-residential/', views.add_residential, name='create_residential'),
+    path('add-new-util/', views.add_utility, name='add_new_util'),
 ]
