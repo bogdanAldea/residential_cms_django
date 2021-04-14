75e77187ce647b4444bf656d885dbe9144b7460e
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-04-14 14:13:51 -0700

    added child form of custom CreateUSerFrom to create tenant objects

--- residential/forms.py
+++ residential/forms.py
@@ -1,12 +1,21 @@
 from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm
-from .models import *
+from django.contrib.auth import get_user_model
+from .models import (
+        Building, Utility
+)
 
+User = get_user_model()
 
 class CreateUSerForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'groups', 'password1', 'password2']
+
+
+class CreateTenant(CreateUSerForm):
+    class Meta(CreateUSerForm.Meta):
+        fields = ['first_name', 'last_name', 'username', 'email', 'phone', 'password1', 'password2']
 
 
 class ResidentialForm(ModelForm):
