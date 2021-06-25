91273d897e004d7e8d5846ca1bba0be01af0ab08
Bogdan <bogdan.aldea0623@gmail.com>
2021-06-25 21:56:33 +0300

    defined a custom user model

--- building/models.py
+++ building/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Defined custom user model
+    """
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
